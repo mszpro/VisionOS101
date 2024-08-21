6acb0227dbefcf5ddd0226e3bad5e1bc0a33eccf
MszPro <code_sign@mszpro.com>
2024-08-21 13:46:32 +0900

    Initial Commit

---
+++ VisionOSDemo/VisionOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSDemoApp.swift
+//  VisionOSDemo
+//
+//  Created by Msz on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
